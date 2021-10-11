813a15a592b7a27f5bb9f8cd715425652f81b675
crisog <ortega.cpp@gmail.com>
2021-10-11 17:04:58 -0400

    feat; platform specific view

--- CustomLiveApp/Views/PlatformSpecificPage.xaml.cs
+++ CustomLiveApp/Views/PlatformSpecificPage.xaml.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace CustomLiveApp.Views
 {
-    public partial class PlatformSpecificPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlatformSpecificPage : TabbedPage
     {
         public PlatformSpecificPage()
         {
