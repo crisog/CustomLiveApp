4e414dada4bc176f66e58ba80d53732b6e67ae92
crisog <ortega.cpp@gmail.com>
2021-10-11 02:42:35 -0400

    feat: add focused effects

---
+++ CustomLiveApp/Effects/FocusEffect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CustomLiveApp.Effects
+{
+    public class FocusEffect : RoutingEffect
+    {
+        public FocusEffect() : base($"MyCompany.{nameof(FocusEffect)}")
+        {
+        }
+    }
+}
