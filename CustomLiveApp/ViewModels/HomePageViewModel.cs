3a3144be897309fc249c969192db86d3505d7f0f
crisog <ortega.cpp@gmail.com>
2021-10-11 01:20:47 -0400

    feat: add commands for all views

--- CustomLiveApp/ViewModels/HomePageViewModel.cs
+++ CustomLiveApp/ViewModels/HomePageViewModel.cs
@@ -8,11 +8,33 @@ namespace CustomLiveApp.ViewModels
     public class HomePageViewModel
     {
         public ICommand DependencyServiceCommand { get; set; }
+        public ICommand CustomRenderCommand { get; set; }
+        public ICommand FocusedEffectCommand { get; set; }
+        public ICommand PlatformSpecificCommand { get; set; }
+
         public HomePageViewModel()
         {
             DependencyServiceCommand = new Command(async () =>
             {
                 await App.Current.MainPage.Navigation.PushAsync(new DependencyServicePage());
+
+            });
+
+            CustomRenderCommand = new Command(async () =>
+            {
+                await App.Current.MainPage.Navigation.PushAsync(new CustomRenderPage());
+
+            });
+
+            FocusedEffectCommand = new Command(async () =>
+            {
+                await App.Current.MainPage.Navigation.PushAsync(new FocusedEffectPage());
+
+            });
+
+            PlatformSpecificCommand = new Command(async () =>
+            {
+                await App.Current.MainPage.Navigation.PushAsync(new PlatformSpecificPage());
 
             });
 
